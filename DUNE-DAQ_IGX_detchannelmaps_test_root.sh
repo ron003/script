8a5b5d93b4717138d05286463200026bbd749190
Ron Rechenmacher <ron@fnal.gov>
2026-06-10 22:17:28 -0500

    fddetdataformats develop requires c++20; Ubuntu 20.04 doesn't do that; checkout hash 4762b2d is before c++20 but pybindsrc is strange

--- DUNE-DAQ_IGX_detchannelmaps_test_root.sh
+++ DUNE-DAQ_IGX_detchannelmaps_test_root.sh
@@ -182,7 +182,9 @@ clone_if_missing DUNE-DAQ/daq-cmake        -b $coredaq_ver
 clone_if_missing DUNE-DAQ/ers              -b coredaq-v5.5.0 #$coredaq_ver
 clone_if_missing DUNE-DAQ/logging          -b coredaq-v5.5.0 #ron/address_warnings #$coredaq_ver
 clone_if_missing DUNE-DAQ/detdataformats   -b coredaq-v5.4.3
-clone_if_missing DUNE-DAQ/fddetdataformats -b fddaq-v5.4.3 # w/ fddaq-v5.4.3, no longer need: sed -i 's/bindings(\*.cpp *)/bindings(*.cpp LINK_LIBRARIES ${PROJECT_NAME})/' fddetdataformats/CMakeLists.txt
+clone_if_missing DUNE-DAQ/fddetdataformats
+cd fddetdataformats; git checkout 4762b2d; rm pybindsrc/{tde*,daphne*,wibeth}.cpp
+cd ..
 #clone_if_missing DUNE-DAQ/detchannelmaps -b $coredaq_ver
 clone_if_missing ron003/detchannelmaps -b ron/run_channel_map_api
 
